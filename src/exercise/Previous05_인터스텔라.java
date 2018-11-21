2a2a825b41a1eee6259a1715266e89571dd6be90
blackMazino <30313742+blackMazino@users.noreply.github.com>
2018-11-21 21:19:19 +0900

    우범지역 완료

--- src/exercise/Previous05_인터스텔라.java
+++ src/exercise/Previous05_인터스텔라.java
@@ -1,5 +1,13 @@
 package exercise;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 public class Previous05_인터스텔라 {
 /*
 [입출력 예]
@@ -48,9 +56,65 @@ public class Previous05_인터스텔라 {
 #10 316086
 
  */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int TC,N,M,K, s,e;//2 ≤ N ≤ 100,000, 1 ≤ M ≤ 200,000, 0 ≤ K ≤ 2
+	static long d[][];
+	static ArrayList<Integer> con[];
+	static ArrayList<Integer> conW[];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		StringTokenizer st;
+		TC = Integer.parseInt(br.readLine());
+		for(int tc=1;tc<=TC;tc++){
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			con = new ArrayList[N+1];
+			conW = new ArrayList[N+1];
+			for(int i=1;i<=N;i++){
+				con[i]=new ArrayList<>();
+				conW[i]=new ArrayList<>();
+			}
+			for(int m=1;m<=M;m++){
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				con[a].add(b); con[b].add(a);
+				conW[a].add(c); conW[b].add(c);
+			}
+			st = new StringTokenizer(br.readLine());
+			s = Integer.parseInt(st.nextToken());
+			e = Integer.parseInt(st.nextToken());
+			
+			d = new long[N+1][K+1];
+			for(int n=1;n<=N;n++){
+				for(int k=0;k<=K;k++){
+					d[n][k] = Long.MAX_VALUE;
+				}
+			}
+			PriorityQueue<Interstela> pq = new PriorityQueue<>();
+			
+		}
 
 	}
 
 }
+class Interstela implements Comparator<Interstela>{
+	public Interstela(long x, long y, long z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	long x,y,z;
+	@Override
+	public int compare(Interstela o1, Interstela o2) {
+		// TODO Auto-generated method stub
+		return (int)(o1.x-o2.x);
+	}
+
+}
+
+
+
--- src/old/그래프_LCA_SdsPre51_LCA_조상이키컸으면.java
+++ src/old/그래프_LCA_SdsPre51_LCA_조상이키컸으면.java
@@ -1,5 +1,4 @@
-package exercise;
-
+package old;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -9,7 +8,7 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class 그래프_LCA_SdsPre51_LCA_조상이키컸으면2 {
+public class 그래프_LCA_SdsPre51_LCA_조상이키컸으면 {
 
 /*
 
@@ -35,7 +34,7 @@ public class 그래프_LCA_SdsPre51_LCA_조상이키컸으면2 {
 #1 3 6 10
 */
 	static int TC, N, Q, K;
-	static int[] H;
+	static int[] height;
 	static int[] answer;
 	static ArrayList<int[]> qList;
 	//LCA
@@ -46,111 +45,121 @@ public class 그래프_LCA_SdsPre51_LCA_조상이키컸으면2 {
 	static int Max = 16;
 	public static void main(String[] args) throws Exception {
 //		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedReader br = new BufferedReader(new FileReader("src/exercise/Previoust51_Input.txt"));
+		BufferedReader br = new BufferedReader(new FileReader("src/exercise/Previoust51.txt"));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		
 		TC = Integer.parseInt(br.readLine());
 		for(int tc=1;tc<=TC;tc++){
 			st = new StringTokenizer(br.readLine());
+			
 			N = Integer.parseInt(st.nextToken());
 			Q = Integer.parseInt(st.nextToken());
-			
-			H = new int[N+1];
-			
+			height = new int[N+1];
+			con = new ArrayList[N+1];
 			depth = new int[N+1];
 			parent = new int [Max+1][N+1];
 			visited = new boolean [N+1];
-			con = new ArrayList[N+1];
-			for(int n=0;n<=N;n++) con[n] = new ArrayList<>();
-			
+			for(int n=0;n<=N;n++) con[n] = new ArrayList<Integer>();
 			for(int n=1;n<=N;n++){
 				st = new StringTokenizer(br.readLine());
 				int p = Integer.parseInt(st.nextToken());
-				int h = Integer.parseInt(st.nextToken());
+				int h  = Integer.parseInt(st.nextToken());				
 				con[n].add(p);
-				con[p].add(n);
-				H[n] = h;
+				con[p].add(n);				
+				parent[0][n] = p;
+				height[n] = h;
 			}
-			
-			depth[1] = 1;
+			depth[1]=1;
 			parent[0][1] = 0;
 			visited[1] = true;
 			dfs(1);
-			//sparse Table
 			for(int k=0;k<=Max-1;k++){
 				for(int n=1;n<=N;n++){
-					parent[k+1][n] = parent[k][parent[k][n]];
-				}
-			}
-			
-			bw.write("#"+tc);
-			
-			int [] t;
+					parent[k+1][n] = parent[k][parent[k][n]]; 
+				}				
+			}			
+			qList = new ArrayList<int[]>();
 			for(int q=1;q<=Q;q++){
 				st = new StringTokenizer(br.readLine());
 				K = Integer.parseInt(st.nextToken());
-				t = new int[K];
-				for(int k=0;k<=K-1;k++){
+				int [] t = new int[K];
+				for(int k=0;k<K;k++){
 					t[k] = Integer.parseInt(st.nextToken());
 				}
+				qList.add(t);
+			}
+			bw.write("#"+tc);			
+			for(int q=0;q<Q;q++){
 				bw.write(" ");
-				bw.write(String.valueOf(getTallestVal(t)));	
+				bw.write(String.valueOf(getMaxHeight(q)));
 			}
 			bw.write("\n");
 			bw.flush();
+//			System.out.println("#"+tc+str);
 		}
-		bw.close();		
+	}
+	private static int getMaxHeight(int q) {
+		int lca = 0;
+//		if(qList.get(q).length==1){
+//			lca = qList.get(q)[0];
+//		}else{
+			lca = getLca(q);
+//		}		
+		return getMaxValue(lca);
 	}
 	
-	private static int getTallestVal(int[] t) {
-		int result = 0;
-		//Lca
-		int lca = t[0];
-		for(int k=0;k<t.length-1;k++){
-//			System.out.println(k+" : "+lca+","+t[k+1]);
-			lca = getLca(lca, t[k+1]);
+	private static int getLca(int q) {
+		int result = qList.get(q)[0];
+		int[] t = qList.get(q);
+		for(int i=0;i<t.length-1;i++){
+			result = lca(result,t[i+1]);
 		}
-		result = H[lca];
-		while(true){
-			result = Math.max(result, H[parent[0][lca]]);
-			lca = parent[0][lca];
-			if(lca == 0) break;
-		}
-		
 		return result;
 	}
-	
-	private static int getLca(int a, int b) {
-		if(depth[a]<depth[b]) return getLca(b,a);
+	private static int lca(int a, int b) {
+		if(depth[b] > depth[a]) return lca(b,a);
 		int d = depth[a] - depth[b];
 		int k=0;
 		while(d>0){
-			if(d%2==1) a = parent[k][a];
+			if(d%2==1) a=parent[k][a];
 			d/=2;
 			k++;
 		}
-		
 		if(a==b) return a;
 		for(int i=Max;i>=0;i--){
-			if(parent[i][a]!=parent[i][b]){
-				a = parent[i][a];
-				b = parent[i][b];
-			}
-		}
+			if(parent[i][a] != parent[i][b]){
+				a=parent[i][a];
+				b=parent[i][b];	
+			}			
+		}		
 		return parent[0][a];
+	}
+	private static int getMaxValue(int lca) {
+		int result = height[lca];
+//		for(int i=0;i<=Max;i++){
+//			if(parent[i][lca]==0){
+//				break;
+//			}
+		
+		while(true){						
+			result = Math.max(result, height[parent[0][lca]]);
+			lca = parent[0][lca];
+			if(lca==0) break;			
+		}
+		return result;
 	}
 	
 	private static void dfs(int i) {
-		for(int n:con[i]){
+		for(int n : con[i]){
 			if(!visited[n]){
-				visited[n]=true;
-				depth[n] = depth[i] +1;
+				visited[n] = true;
+				depth[n] = depth[i]+1;
 				parent[0][n] = i;
-				dfs(n);						
+				dfs(n);
 			}
 		}
 		
 	}
 
 }
+
